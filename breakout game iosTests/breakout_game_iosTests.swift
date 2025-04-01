95eae9a53e02b15d4e1f54dfcdfa543774c1cfbc
Samante <samante@bsd220.org>
2025-04-01 18:39:06 -0500

    Initial Commit

---
+++ breakout game iosTests/breakout_game_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  breakout_game_iosTests.swift
+//  breakout game iosTests
+//
+//  Created by Samuel Amante on 4/1/25.
+//
+
+import Testing
+@testable import breakout_game_ios
+
+struct breakout_game_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
